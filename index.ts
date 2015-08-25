8bbe88ebdf3854e89c6dcb6b9e46dee72a05ec70
tkQubo <tk.qubo@gmail.com>
2015-08-26 01:33:22 +0900

    Load remote json file

--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
-console.log('hello world');
+import * as request from 'request';
+import * as http from 'http';
+
+
+let uri = 'http://raw.githubusercontent.com/aws/aws-sdk-js/master/apis/ec2-2015-04-15.normal.json';
+request.get(uri, (err: any, res: http.IncomingMessage, body: any) => {
+    let json: any = JSON.parse(body);
+    console.log(json.version);
+});
